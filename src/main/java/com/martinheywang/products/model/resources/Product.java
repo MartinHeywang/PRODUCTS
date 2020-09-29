9279c74f50e779918f18d48102e6a86d6b8e79f2
Martin HEYWANG <martin.heywang1@gmail.com>
2020-09-29 07:17:46 +0200

    Adding resources

--- src/main/java/com/martinheywang/products/model/resources/Product.java
+++ src/main/java/com/martinheywang/products/model/resources/Product.java
@@ -61,6 +61,10 @@ public enum Product implements Resource {
 			@RemotePack(clazz = Ore.class, field = "ALUMINIUM", quantity = "5") })
 	BATTERY("Pile", "1050", "/Pile.png"),
 
+	@Craftable(recipe = { @RemotePack(clazz = Product.class, field = "BATTERY"),
+			@RemotePack(clazz = Plate.class, field = "PLATE_OF_IRON") })
+	RADIO("Radio", "1200", "/None.png"),
+
 	@Craftable(recipe = { @RemotePack(clazz = Product.class, field = "COOLER_PLATE", quantity = "2"),
 			@RemotePack(clazz = Plate.class, field = "PLATE_OF_SILVER", quantity = "8") })
 	FRIDGE("Réfrigérateur", "1500", "/None.png"),
@@ -77,17 +81,41 @@ public enum Product implements Resource {
 			@RemotePack(clazz = Product.class, field = "CIRCUIT", quantity = "2") })
 	SOLAR_PANEL("Panneau solaire", "2000", "/Panneau_Solaire.png"),
 
-	@Craftable(recipe = { @RemotePack(clazz = Plate.class, field = "PLATE_OF_SILVER", quantity = "5"),
-			@RemotePack(clazz = Product.class, field = "CIRCUIT", quantity = "3") })
-	ENGINE("Moteur", "2000", "/Moteur.png"),
+	@Craftable(recipe = { @RemotePack(clazz = Plate.class, field = "PLATE_OF_IRON"),
+			@RemotePack(clazz = Wire.class, field = "COPPER_WIRE") })
+	POWER_SUPPLY("Alimentation", "1500", "/None.png"),
+
+	@Craftable(recipe = { @RemotePack(clazz = Plate.class, field = "PLATE_OF_ALUMINIUM", quantity = "2"),
+			@RemotePack(clazz = Product.class, field = "ANTENNA") })
+	MICROWAVE("Micro-ondes", "2500", "/None.png"),
 
 	@Craftable(recipe = { @RemotePack(clazz = Product.class, field = "PROCESSOR"),
-			@RemotePack(clazz = Plate.class, field = "PLATE_OF_ALUMINIUM", quantity = "5") })
+			@RemotePack(clazz = Product.class, field = "POWER_SUPPLY") })
 	COMPUTER("Ordinateur", "3000", "/None.png"),
 
-	@Craftable(recipe = { @RemotePack(clazz = Ore.class, field = "GOLD", quantity = "7"),
-			@RemotePack(clazz = Ingot.class, field = "GOLD_INGOT", quantity = "6") })
-	ICON("PRODUCTS.", "5000", "/None.png");
+	@Craftable(recipe = { @RemotePack(clazz = Product.class, field = "PROCESSOR"),
+			@RemotePack(clazz = Plate.class, field = "PLATE_OF_ALUMINIUM") })
+	DRONE("Drone", "4000", "/None.png"),
+
+	@Craftable(recipe = { @RemotePack(clazz = Plate.class, field = "PLATE_OF_SILVER", quantity = "5"),
+			@RemotePack(clazz = Product.class, field = "CIRCUIT", quantity = "3") })
+	ENGINE("Moteur", "5000", "/Moteur.png"),
+
+	@Craftable(recipe = { @RemotePack(clazz = Product.class, field = "POWER_SUPPLY"),
+			@RemotePack(clazz = Plate.class, field = "PLATE_OF_IRON") })
+	TV("TV", "7000", "/None.png"),
+
+	@Craftable(recipe = { @RemotePack(clazz = Product.class, field = "POWER_SUPPLY"),
+			@RemotePack(clazz = Ore.class, field = "GOLD"), @RemotePack(clazz = Wire.class, field = "COPPER_WIRE") })
+	GENERATOR("Générateur", "9000", "/None.png"),
+
+	@Craftable(recipe = { @RemotePack(clazz = Product.class, field = "HOTPLATE", quantity = "2"),
+			@RemotePack(clazz = Plate.class, field = "PLATE_OF_ALUMINIUM", quantity = "5") })
+	OVEN("Four", "10000", "/None.png"),
+
+	@Craftable(recipe = { @RemotePack(clazz = Ore.class, field = "GOLD", quantity = "10"),
+			@RemotePack(clazz = Ingot.class, field = "GOLD_INGOT", quantity = "7") })
+	PRODUCTS("Icon", "30000", "/None.png");
 
 	private String name;
 	private BigInteger price;
